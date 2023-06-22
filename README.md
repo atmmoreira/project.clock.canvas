e7f796e71d9de2fef3bba7f5ca7db76a491cc8f6
Anderson Martins <atmmoreira.rj@gmail.com>
2023-06-22 08:19:40 -0300

    finish project

---
+++ README.md
@@ -0,0 +1,59 @@
+<div align="center">
+  <img src="readme/logo/favicon.png" />
+  <h1>Anderson Toledo Martins Moreira</h1>
+  <h4>
+    Frontend Software Engineer » Salesforce Administrator & Developer <br />
+    <a href="http://www.atmm.dev" target="_blank">Resume Online</a> -
+    <a href="https://www.linkedin.com/in/atmmoreira" target="_blank">Linkedin</a> -
+    <a href="https://trailblazer.me/id/atmmdev" target="_blank">Trailblazer</a>
+  </h4>
+</div>
+<br >
+<!-- References for Create budgets :: https://shields.io/category/build -->
+<div align="center">
+  <img src="https://img.shields.io/static/v1?label=STATUS&message=COMPLETE&color=blue&style=for-the-badge"/>
+  <img src="https://img.shields.io/static/v1?label=CSS&message=3&color=pink&style=for-the-badge"/>
+  <img src="https://img.shields.io/static/v1?label=Javascript&message=es6&color=yellow&style=for-the-badge"/>
+  <img src="https://img.shields.io/static/v1?label=CANVAS&message=1.0&color=red&style=for-the-badge"/>
+</div>
+
+<br >
+
+## Description
+This is a project to improve my skills with Javascript Vanilla with canvas, follow the tutorial from Brad Traversy.
+
+## Screens Layout
+<div align="center">
+  Layout <br><br>
+  <img src="readme/layout/layout.jpg" alt="Login"/>
+</div>
+
+<br >
+
+## Programming Languages and Frameworks.
+```bash
+# HTML5 / CSS3
+# Javascript
+# Git / Github
+# Canvas
+```
+
+<br >
+
+## System Requirements
+```bash
+# Git
+# Node
+```
+
+<br >
+
+## Getting Started With Local Development Configuration
+
+```bash
+# Download or clone the repository.
+# Open the project inside your favorite IDE (I use VSCode).
+# If you have the extension `Live Server` installed in VSCode.
+# Click on the file index.html with right button in yor mouse, and select `Open with Live server`
+# If NOT, you just open the file index.html in your favorite browser.
+```
---
+++ main.js
@@ -0,0 +1,105 @@
+function clock() {
+  const now = new Date();
+  const canvas = document.getElementById('canvas');
+  const ctx = canvas.getContext('2d');
+
+  // Setup Canvas
+  // Save the Default State
+  ctx.save();
+  ctx.clearRect(0, 0, 500, 500);
+  ctx.translate(250, 250); // Put 0, 0 in the  middle
+  ctx.rotate(-Math.PI / 2); // Rotate Clock -90deg
+
+  // Set Default Styles
+  ctx.strokeStyle = '#34495e';
+  ctx.fillStyle = '#ecf0f1';
+  ctx.lineWidth = 5;
+  ctx.lineCap = 'round';
+
+  // Draw Clock Face/Border
+  ctx.save();
+  ctx.beginPath();
+  ctx.lineWidth = 10;
+  ctx.strokeStyle = '#2c3e50';
+  ctx.arc(0, 0, 142, 0, Math.PI * 2, true);
+  ctx.stroke();
+  ctx.fill();
+  ctx.restore();
+
+  // Draw Hour Lines
+  ctx.save();
+  for (let i = 0; i < 12; i++) {
+    ctx.beginPath();
+    ctx.rotate(Math.PI / 6);
+    ctx.moveTo(100, 0);
+    ctx.lineTo(120, 0);
+    ctx.stroke();
+  }
+  ctx.restore();
+
+  // Draw Minutes Lines
+  ctx.save();
+  ctx.lineWidth = 4;
+  ctx.strokeStyle = '#7f8c8d';
+  for (let i = 0; i < 60; i++) {
+    if (i % 5 !== 0) {
+      ctx.beginPath();
+      ctx.moveTo(117, 0);
+      ctx.lineTo(120, 0);
+      ctx.stroke();
+    }
+    ctx.rotate(Math.PI / 30);
+  }
+  ctx.restore();
+
+  // Get Current Time
+  const hr = now.getHours() % 12;
+  const min = now.getMinutes();
+  const sec = now.getSeconds();
+
+  console.log(`${hr}:${min}:${sec}`);
+
+  // Draw Hour Hand
+  ctx.save();
+  ctx.rotate(
+    (Math.PI / 6) * hr + (Math.PI / 360) * min + (Math.PI / 21600) * sec
+  );
+  ctx.strokeStyle = '#800000';
+  ctx.lineWidth = 6;
+  ctx.beginPath();
+  ctx.moveTo(-20, 0);
+  ctx.lineTo(80, 0);
+  ctx.stroke();
+  ctx.restore();
+
+  // Draw Minutes Hand
+  ctx.save();
+  ctx.rotate((Math.PI / 30) * min + (Math.PI / 1800) * sec);
+  ctx.strokeStyle = '#800000';
+  ctx.lineWidth = 5;
+  ctx.beginPath();
+  ctx.moveTo(-28, 0);
+  ctx.lineTo(115, 0);
+  ctx.stroke();
+  ctx.restore();
+
+  // Draw Seconds Hand
+  ctx.save();
+  ctx.rotate((sec * Math.PI) / 30);
+  ctx.strokeStyle = '#e74c3c';
+  ctx.fillStyle = '#e74c3c';
+  ctx.lineWidth = 2;
+  ctx.beginPath();
+  ctx.moveTo(-30, 0);
+  ctx.lineTo(100, 0);
+  ctx.stroke();
+  ctx.beginPath();
+  ctx.arc(0, 0, 5, 0, Math.PI * 2, true);
+  ctx.fill();
+  ctx.restore();
+
+  // Restore Default State
+  ctx.restore();
+  requestAnimationFrame(clock);
+}
+requestAnimationFrame(clock);
